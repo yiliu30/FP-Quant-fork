d1b9c0150970791cc3e4123dddaa9a2d02ca7f83
Andrei Panferov <andrei@panferov.org>
2025-07-14 18:35:16 +0200

    0.1.3

--- inference_lib/setup.py
+++ inference_lib/setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="fp_quant",
-    version="0.1.2",
+    version="0.1.3",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
     author="Andrei Panferov",
